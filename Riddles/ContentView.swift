36b1770343e1d1f252f91c2d42f4284517288e7e
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-08-16 11:26:34 +0300

    Initial Commit

---
+++ Riddles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Riddles
+//
+//  Created by Jassem Al-Buloushi on 8/16/20.
+//  Copyright © 2020 Jassem Al-Buloushi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
