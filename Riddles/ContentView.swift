7c162a851c6b99da5d7db90405caf789e13c3c3d
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-08-16 12:07:15 +0300

    - Create the Riddle app basic UI

--- Riddles/ContentView.swift
+++ Riddles/ContentView.swift
@@ -9,9 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSettings = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            GeometryReader { geomerty in
+                VStack {
+                    Text("What has to be broken before you can use it?")
+                        .frame(width: geomerty.size.width, height: geomerty.size.height / 2)
+                    
+                    Button("Show Answer") {
+                        // some code
+                    }
+                    .frame(width: geomerty.size.width / 3, height: 40)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .padding()
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button(action: {
+                            //Action Code
+                        }) {
+                            Image(systemName: "chevron.left.circle")
+                                .font(.system(size: 40))
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Action code
+                            
+                        }) {
+                            Image(systemName: "chevron.right.circle")
+                                .font(.system(size: 40))
+                        }
+                    }
+                    .padding([.horizontal, .vertical])
+                }
+            }
+            .padding()
+            .navigationBarTitle("Riddles", displayMode: .large)
+            .navigationBarItems(trailing: Button(action:{
+            self.showSettings = true
+            }) {
+                Image(systemName: "line.horizontal.3")
+                    .font(.system(size: 30))
+            })
+        }
+    
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
